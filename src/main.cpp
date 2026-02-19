281b39e6160e349d52726079f114160b345d2142
chase <sam8poop@gmail.com>
2026-02-20 07:38:52 +1030

    :3

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_LEDBackpack.h>
+#include <Servo.h>
+bool a = false;
+Servo servo;
 Adafruit_LEDBackpack matrix = Adafruit_LEDBackpack();
 bool rows_and_cols[8][8];
 void toggle_led(int col,int row) {rows_and_cols[col][row] = !rows_and_cols[col][row];}
@@ -10,11 +13,21 @@ void display() {
     matrix.writeDisplay();
 }
 void setup() {
+    servo.attach (11);
     matrix.begin(0x70);
     matrix.setBrightness(1);
-}
-void loop() {
+    
     for (int i = 0;i<9;i++) {toggle_led(i,3);}toggle_led(4,1);toggle_led(4,5);toggle_led(5,1);toggle_led(5,5);toggle_led(5,2);toggle_led(5,4);toggle_led(6,2);toggle_led(6,4); // arrow
     display();
+}
+void loop() {
     delay(1000);
+    if (a) {
+        servo.write(100);
+        a = !a;
+    }
+    else {
+        servo.write(0);
+        a = !a;
+    }
 }
