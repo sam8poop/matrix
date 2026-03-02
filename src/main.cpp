6b11eb00e9cb1455830460f2d5bdaf3c8fca66a8
chase <sam8poop@gmail.com>
2026-03-02 11:30:03 +1030

    i did stuf

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,27 @@
 #include <Arduino.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_LEDBackpack.h>
+#include <happen.h>
+#include <limit.h>
 Adafruit_LEDBackpack matrix = Adafruit_LEDBackpack();
+// vars 
+unsigned long cmils;
 bool rows_and_cols[8][8];
+// structs
+struct button{unsigned long mills;int prid = 10;bool current_button_sate;bool last_button_state;int pin;button(int button_pin) {pin = button_pin;pinMode(pin,INPUT_PULLUP);}};
+struct dot {int r = 7;int c = 0;button left, up, down, right;dot(int l, int u, int d, int r): left(l), up(u), down(d), right(r){}}left(11,10,9,8),right(5,4,3,2);
+// function declarations
+void update(dot& z);
+void toggle_led(int col,int row);
+bool button_test(button &z);
+void display();
+void read_pin(button &z);
+// setup and loop
+void setup() {matrix.begin(0x70);matrix.setBrightness(1);}
+void loop() {cmils = millis();update(left);update(right);}
+// function definitions
+void update(dot& z) {if (button_test(z.left)) {z.c--;}if (button_test(z.right)) {z.c++;}if (button_test(z.up)) {z.r++;}if (button_test(z.down)) {z.r--;}limit(z.r,7);limit(z.c,7);toggle_led(z.c,z.r);display();toggle_led(z.c,z.r);read_pin(z.left);read_pin(z.right);read_pin(z.up);read_pin(z.down);    }
 void toggle_led(int col,int row) {rows_and_cols[col][row] = !rows_and_cols[col][row];}
-int get_final_val(int col) {return 128*rows_and_cols[col][0]+1*rows_and_cols[col][1]+2*rows_and_cols[col][2]+4*rows_and_cols[col][3]+8*rows_and_cols[col][4]+16*rows_and_cols[col][5]+32*rows_and_cols[col][6]+64*rows_and_cols[col][7];}
-void display() {
-    for (int i = 0;i<8;i++) {matrix.displaybuffer[i] = get_final_val(i);}
-    matrix.writeDisplay();
-}
-void setup() {
-    matrix.begin(0x70);
-    matrix.setBrightness(1);
-    
-
-}
-void loop() {
-    for (int i = 0;i<9;i++) {toggle_led(i,3);}toggle_led(4,1);toggle_led(4,5);toggle_led(5,1);toggle_led(5,5);toggle_led(5,2);toggle_led(5,4);toggle_led(6,2);toggle_led(6,4); // arrow
-    display();
-    delay(1000);
-}
+bool button_test(button& z) {if(happen(cmils,z.mills,z.prid,false) && z.last_button_state == false && z.current_button_sate == true) {z.mills = cmils;z.last_button_state = true;return true;}z.last_button_state = z.current_button_sate;return false;}
+void display() {for (int col = 0; col < 8; col++) {uint8_t value = 0;for (int row = 0; row < 8; row++) {value |= (rows_and_cols[col][row] << row);}matrix.displaybuffer[col] = value;}matrix.writeDisplay();}
+void read_pin(button& z) {z.current_button_sate = !digitalRead(z.pin);}
